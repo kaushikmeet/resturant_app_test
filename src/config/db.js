d526105a8edb73935226f31230161b3c56141823
Dew <maidoradewharfan@gmail.com>
2025-12-10 13:21:01 +0530

    update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
+  host: 'localhost',
   user: 'root',
   password: '',
-  port: 3306,
   database: 'restaurant_app',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
 });
 
 module.exports = pool;
